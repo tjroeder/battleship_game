2ad1c0544eee0ec3ce3e149d69228589934b39bb
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-10-18 20:22:08 -0600

    Feat: created Ship class #hit

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,8 +7,13 @@ class Ship
     @health = length
   end
 
+  # Return true if the ship is sunk and has zero health.
   def sunk?
     @health == 0
   end
 
+  # Hit to remove health from the ship.
+  def hit
+    @health -= 1
+  end
 end
