dd6e60f63eaa63fb5781c4e651d6c36b07c7419d
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-10-18 20:21:29 -0600

    Feat: created tests for #hit

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -29,20 +29,40 @@ RSpec.describe Ship do
   end
 
   describe '#sunk?' do
+    # Test if Ship is initially not sunk.
     it 'initially is not sunk' do
       expect(@cruiser.sunk?).to eq(false)
     end 
 
+    # Test if Ship is hit it will not sink if it still has health.
     it 'can be hit' do
       @cruiser.hit
       expect(@cruiser.sunk?).to eq(false)
     end
 
+    # Test that if hit enough so health == 0 the Ship is sunk.
     it 'can be sunk' do
       @cruiser.hit
       @cruiser.hit
       @cruiser.hit
       expect(@cruiser.sunk?).to eq(true)
+    end
+  end
+
+  describe '#hit' do
+    # Before being hit, the Ship is at full health.
+    it 'initially has no hits' do
+      expect(@cruiser.health).to eq(3)
+    end
+
+    # Each hit removes health.
+    it 'can remove health' do
+      @cruiser.hit
+      expect(@cruiser.health).to eq(2)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(1)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(0)
     end
   end
 end
