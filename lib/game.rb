edf5d9f082020d28bac39ccb745806924266bdbb
Jimmy Page <jpage@knights.ucf.edu>
2021-10-25 16:53:35 -0600

    Feat: set up game shell

--- lib/game.rb
+++ lib/game.rb
@@ -27,10 +27,88 @@ class Game
   def user_input
     input = gets.chomp
   end
+  
+  def board_hash
+  end
 
   def start_game
-    puts "PLACE SHIPS"
-    puts "I have laid out my ships on the grid."
-    puts "You now need to lay out your two ships."
+    require "pry"; binding.pry
+    "PLACE SHIPS\n" +
+    "I have laid out my ships on the grid.\n" +
+    "You now need to lay out your two ships.\n" +
+    "The Cruiser is three units long and the Submarine is two units long.\n" +
+    "#{board_hash}\n" +
+    "Enter the squares for the Cruiser (3 spaces)\n>"
   end
+
+  def check_user_coordinates
+    if @player_cruiser.input = valid_placement?
+      @player_cruiser.place_ship
+    elsif
+      "Enter the squares for the Cruiser (3 spaces):\n>"
+    end
+
+    if @player_submarine.input = valid_coordinate?
+      @player_submarine.place_ship
+    elsif
+      "Enter the squares for the Submarine (2 spaces):\n>"
+    end
+    #loop until valid coordinates
+    #if coordinates are valid, place cruiser first
+    #then place submarine
+    #if coordinates are not valid, puts "Those are invalid coordinates please try again"
+  end
+
+  def setup_board(input)
+    #npc places ships randomized
+    #user places ships game.user_board
+    #puts npc board game.npc_board
+    #puts user board
+    #puts board.render
+  end
+
+  def turn
+    #Displaying the boards
+# Player choosing a coordinate to fire on
+# Computer choosing a coordinate to fire on
+# Reporting the result of the Player’s shot
+# Reporting the result of the Computer’s shot
+  end
+
+  def display
+    #display computer board (without ship placement)
+    #display player board (with ship placement)
+  end
+
+  def player_shot
+    #ask for coordinate to fire on
+    #loop until valid coordinate
+  end
+
+  def npc_shot
+    #computer chooses random valid coordinate that has not been chosen yet
+  end
+
+  def results
+    #puts results of player shot: "your shot on A4 was a miss"
+    #puts results of computer shot: "my shot on C1 was a miss"
+    #handle missed shots
+    #handle hit shots: shot was a hit
+    #handle sunk ships: shot was a hit and you sunk my ship
+  end
+
+  def player_shot
+    #player enters coordinates
+    #if invalid, re enter (loop?)
+    #if valid but already fired upon, re enter valid coordinate that has not been fired upon
+  end
+
+  def end_game
+    #diplay "You won!"
+    # or display "I won!"
+    #then return to main menu
+  end
+
+
+
 end
