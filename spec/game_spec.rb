82355c67eb6a2817abd4a57142d722289806899e
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-10-26 15:22:34 -0600

    Test: created tests for Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,19 +2,38 @@ require './lib/board'
 require './lib/game'
 
 RSpec.describe Game do
-  let!(:user_board) {Board.new}
-  let!(:npc_board) {Board.new}
-  let!(:game) {Game.new(user_board, npc_board)}
+  let!(:game) {Game.new}
 
   describe '#initialize' do
     it 'exists' do
-      # game = Game.new(user_board, npc_board)
       expect(game).to be_instance_of(Game)
     end
 
-    it 'creates user and npc boards' do
-      expect(game.user_board).to be_instance_of(Board)
-      expect(game.npc_board).to be_instance_of(Board)
+    it 'creates blank user board and npc instance' do
+      expect(game.user_board).to eq('')
+      expect(game.npc).to eq('')
+    end
+
+    it 'creates empty ship arrays' do
+      expect(game.user_ships).to eq([])
+      expect(game.npc_ships).to eq([])
+    end
+
+    it 'creates num_to_name hash' do
+      expected = {  
+                  10 => 'ten',
+                   9 => 'nine',
+                   8 => 'eight',
+                   7 => 'seven',
+                   6 => 'six',
+                   5 => 'five',
+                   4 => 'four',
+                   3 => 'three',
+                   2 => 'two',
+                   1 => 'one',
+                   0 => 'zero'
+                 }
+      expect(game.num_to_name).to eq(expected)
     end
   end
 
@@ -25,12 +44,6 @@ RSpec.describe Game do
   end
 
   describe '#main_menu_check' do
-    # it 'quits the game' do
-    #   user_input = 'q'
-
-    #   expect(game.main_menu_check(user_input)).to raise_error(SystemError)
-    # end
-
     it 'recognizes wrong input' do
       user_input = '7'
 
@@ -43,26 +56,96 @@ RSpec.describe Game do
       user_input = 'asdfasdoufadshfgk'
       expect(game.main_menu_check(user_input)).to eq(expected)
     end
+
+    it 'recognizes play input' do
+      expect(game.main_menu_check('p')).to eq(true)
+    end
   end
 
   describe '#start_game' do
     it 'prints required text to the user' do
-      expected = "PLACE SHIPS\n" +
+      game.create_objects
+      
+
+      expected =
       "I have laid out my ships on the grid.\n" +
       "You now need to lay out your two ships.\n" +
       "The Cruiser is three units long and the Submarine is two units long.\n" +
-      "#{board_hash}\n" +
-      "Enter the squares for the Cruiser (3 spaces)\n>"
-
+      "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
       expect(game.start_game).to eq(expected)
     end
   end
-end
 
-#   describe '#check_user_coordinates' do
-#     it 'can confirm if input is valid'
-#     # if valid, place ship
-#     #if not valid, ask again to place ship
-#
-#   end
-# end
+  describe '#user_placement_selection' do
+    it 'prints the name of the ship and length' do
+      game.create_objects
+
+      expected = "Enter the squares for the Cruiser (3 spaces):\n> "
+      expect(game.user_placement_selection(game.user_ships.first)).to eq(expected)
+      expected = "Enter the squares for the Submarine (2 spaces):\n> "
+      expect(game.user_placement_selection(game.user_ships.last)).to eq(expected)
+    end
+  end
+
+  describe '#convert_user_coord' do
+    it 'it takes in coordinates' do
+      expected = ['A1', 'A2', 'A3']
+      expect(game.convert_user_coord('A1 A2 A3')).to eq(expected)
+    end
+  end
+
+  describe '#place_user_ship' do 
+    it 'returns false for out of bounds coordinates' do
+      game.create_objects
+      coord = ['@1', 'A1', 'B1']
+      expect(game.place_user_ship(game.user_ships[0], coord)).to eq(false)
+    end
+
+    it 'returns false for invalid wrong length coordinates' do
+      game.create_objects
+      coord = ['A1', 'B1']
+      expect(game.place_user_ship(game.user_ships[0], coord)).to eq(false)
+    end
+
+    it 'returns false for diagonal ships' do
+      game.create_objects
+      coord = ['A1', 'B2', 'C3']
+      expect(game.place_user_ship(game.user_ships[0], coord)).to eq(false)
+    end
+
+    it 'returns false for non consecutive coordinates' do
+      game.create_objects
+      coord = ['A1', 'A2', 'A4']
+      expect(game.place_user_ship(game.user_ships[0], coord)).to eq(false)
+      coord = ['A1', 'B1', 'D1']
+      expect(game.place_user_ship(game.user_ships[0], coord)).to eq(false)
+    end
+
+    it 'places the ship if all coordinates for the ship are valid' do
+      game.create_objects
+      coord = ['A1', 'A2', 'A3']
+      game.place_user_ship(game.user_ships[0], coord)
+      expect(game.user_board.board_hash['A1'].empty?).to eq(false)
+      expect(game.user_board.board_hash['A2'].empty?).to eq(false)
+      expect(game.user_board.board_hash['A3'].empty?).to eq(false)
+    end
+  end
+
+  describe '#invalid_coord' do
+    it 'returns string' do
+      expected = "Those are invalid coordinates.\nPlease try again:\n> "
+      expect(game.invalid_coord).to eq(expected)
+    end
+  end
+
+  describe '#show_user_ships_setup' do
+    it 'returns render with user ships' do
+      game.create_objects
+      coord = ['A1', 'A2', 'A3']
+      game.place_user_ship(game.user_ships[0], coord)
+
+      expected = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+      expect(game.show_user_ships_setup).to eq(expected)
+    end
+  end
+end
