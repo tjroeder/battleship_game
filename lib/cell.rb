01bc1fad2264e62b713c78f87e6fcc9011021049
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-10-27 11:52:43 -0600

    Fix: tweaked comment spacing and standardized

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,41 +6,46 @@ class Cell
     @ship = nil
     @shot_status = false
   end
-  #cell is empty if there is no ship
+
+  # Cell is empty if there is no ship.
   def empty?
     @ship.nil?
   end
-  #this method helps place ships on a cell
+
+  # This method helps place ships on a cell.
   def place_ship(ship)
     @ship = ship
   end
-  #this method shows the status of a shot/ wether or not a cell has been shot at
+
+  # This method shows the status of a shot/ wether or not a cell has been shot at.
   def fired_upon?
     @shot_status
   end
-  #this method contains the actual action of shooting on a cell
-  #if the cell has a ship, and is fired upon, the ship is hit
+
+  # This method contains the actual action of shooting on a cell.
+  # If the cell has a ship, and is fired upon, the ship is hit.
   def fire_upon
     if !empty?
       @ship.hit
     end
     @shot_status = true
   end
-  #this render shows the possible outcomes of a shot
+
+  # This render shows the possible outcomes of a shot.
   def render(show = false)
-    #M is a missed shot
+    # M is a missed shot.
     if empty? && fired_upon?
       "M"
-    #H is a hit
+    # H is a hit.
     elsif fired_upon? && !@ship.sunk?
       "H"
-    #the hits will turn to Xs once the ship has sunk
+    # Hits will turn to Xs once the ship has sunk.
     elsif fired_upon? && @ship.sunk?
       "X"
-      #the S shows the user where their ships are placed
+    # The S shows the user where their ships are placed.
     elsif !fired_upon? && !empty? && show
       "S"
-      #the . is the standard initial cell status 
+    # '.' is the standard initial cell status.
     else
       "."
     end
